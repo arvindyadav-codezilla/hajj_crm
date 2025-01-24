cca25c4d3fd4900fd20a1683ac45d556549189e7
Arvind Yadav <arvind.yadav@codezilla.io>
2025-01-24 20:02:14 +0530

    add latest build

---
+++ .angular/cache/18.2.12/hajj-crm-frontend/vite/deps/@angular_animations.js
@@ -0,0 +1,50 @@
+import {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  BrowserAnimationBuilder,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-MYSYXHM6.js";
+import "./chunk-XNLLRQJ5.js";
+import "./chunk-E24KTSAE.js";
+import "./chunk-6TIMDMPZ.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
+//# sourceMappingURL=@angular_animations.js.map
